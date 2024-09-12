55d82679a03a66caefe05ad73b76307208888daf
Dylan-Trows <TRWDYL001@myuct.ac.za>
2024-09-12 11:41:57 +0200

    adding example waypoint, removing commented code from episode logic and prior custom message, calculating heading error, updating reward function to reward distance to target + low heading error + low energy use

--- src/catarob_drl/catarob_drl/VRX_Controller.py
+++ src/catarob_drl/catarob_drl/VRX_Controller.py
@@ -1,9 +1,3 @@
-# # VRXStepData.msg
-# float32[] state
-# float32 reward
-# bool done
-# int32 episode_step
-
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
@@ -35,25 +29,24 @@ class VRXController(Node):
         
         self.waypoint_manager = WaypointManager()
         #TODO Add Example Waypoints here !
+        self.waypoint_manager.add_waypoint(-33.722499646, 150.674243934, 1.161502034)                                  # Example waypoint in VRX
 
         # Use sensor_qos for sensor data subscriptions
         self.create_subscription(NavSatFix, '/wamv/sensors/gps/gps/fix', self.gps_callback, sensor_qos)
         self.create_subscription(Imu, '/wamv/sensors/imu/imu/data', self.imu_callback, sensor_qos)
         
         # Use reliable_qos for task info and action subscriptions
-        #self.create_subscription(Task, '/vrx/task/info', self.task_info_callback, reliable_qos)                       #TODO
+        #self.create_subscription(Task, '/vrx/task/info', self.task_info_callback, reliable_qos)                       
         self.create_subscription(Float64MultiArray, '/vrx/action', self.action_callback, reliable_qos)                #TODO
         
         # Use reliable_qos for publishers
         self.left_thruster_pub = self.create_publisher(Float64, '/wamv/thrusters/left/thrust', reliable_qos)
         self.right_thruster_pub = self.create_publisher(Float64, '/wamv/thrusters/right/thrust', reliable_qos)
-        #self.state_pub = self.create_publisher(Float32MultiArray, '/vrx/state', 10)
-        #self.reward_pub = self.create_publisher(Float64MultiArray, '/vrx/reward', 10)
         self.step_publisher = self.create_publisher(VRXStepData, '/vrx/step_data', reliable_qos)
 
         
         self.create_timer(0.1, self.control_loop)                                                           # Control logic loop timer 
-                                                                                                            #TODO  change if needed
+                                                                                                            #TODO  change loop frquency if needed
         # Initialise 
         self.current_heading = 0.0
         self.gps_data = None
@@ -61,30 +54,34 @@ class VRXController(Node):
         self.current_waypoint = None
         #self.current_state = None
         self.last_action = None
-        #self.episode_step = 0
-        #self.max_steps = 1000
         self.task_finished = False
         self.heading_error = 0.0
+        self.previous_distance = None
 
         
     def gps_callback(self, msg):                                                                            # handle incomming GPS data from environment 
         self.gps_data = [msg.latitude, msg.longitude, msg.altitude]
-        self.waypoint_manager.update_position(self.gps_data)                                                # update waypoint manager with new info
+
+        print("lat: ",msg.latitude," lon: ",msg.longitude," alt: ", msg.altitude)                           # Test print statement
+
+        self.waypoint_manager.update_position(msg.latitude, msg.longitude, msg.altitude)                    # update waypoint manager with new info
         current_waypoint = self.waypoint_manager.get_current_waypoint()                                     # Get the current waypoint
         if current_waypoint:
-            desired_heading = self.waypoint_manager.get_desired_heading(self.current_lat, self.current_lon)
-
+            desired_heading = self.waypoint_manager.get_desired_heading(msg.latitude, msg.longitude)
+            print("desired heading : ", desired_heading)
+            print("current heading : ", self.current_heading)
             # Calculate heading error
-            self.heading_error = self.waypoint_manager.calculate_heading_error(self.current_heading, desired_heading)
+            self.heading_error = self.waypoint_manager.calculate_heading_error(self.current_heading, desired_heading)       # Get Heading error for Reward function
 
         # Check if we've reached all waypoints
         if not self.waypoint_manager.has_more_waypoints():
-            self.task_finished = True
+            self.task_finished = True                                                                       # means that we have reached the Waypoint and the Episode is over !
+            # Success!
 
     def imu_callback(self, msg):                                                                            # handles incomming IMU data from environment 
         self.imu_data = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]
         # Extract heading from quaternion
-        x, y, z, w = msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w
+        x, y, z, w = msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w             #TODO DECIDE Angular velocity, Linear Acceleration
         self.current_heading = math.degrees(math.atan2(2 * (w * z + x * y), 1 - 2 * (y * y + z * z)))
         self.current_heading = (self.current_heading + 360) % 360                                                                                                    
         
@@ -94,15 +91,33 @@ class VRXController(Node):
     def action_callback(self, msg):                                                                         # Receives Action from DRL Agent 
         self.last_action = msg.data
         
-    def get_reward(self):                                                                                  #TODO Implement my own Reward function class 
-        reward = 0
+    def get_reward(self):                                                                                   #TODO Implement my own Reward function class 
+        reward = 0.0
         # Reward for being on the correct heading
-        reward += math.cos(math.radians(self.heading_error))
-        
+        #reward += math.cos(math.radians(self.heading_error))
+        if self.heading_error < 10:
+            reward += 1
+        else :
+            reward -= 1
+        # 2. Reward for getting closer to the target                                                        # TODO Implement logic to compare distance to Original distance!
+        if self.previous_distance is not None:
+            distance_to_target = self.waypoint_manager.calculate_distance(self.gps_data[0],self.gps_data[1], self.current_waypoint[0] , self.current_waypoint[1])
+            distance_reward = self.previous_distance - distance_to_target
+            reward += 5 * distance_reward  # Scale factor of 5 to emphasize progress towards the goal
+            self.previous_distance = distance_to_target
+        else:
+            if self.current_waypoint is not None:
+                self.previous_distance = self.waypoint_manager.calculate_distance(self.gps_data[0],self.gps_data[1], self.current_waypoint[0] , self.current_waypoint[1])
+
+        # 3. Penalty for energy usage (assuming self.last_action contains thruster values)                  # TODO Think of logic for energy consumption / smoothness of trajectories
+        if self.last_action is not None:
+            energy_usage = np.sum(np.abs(self.last_action))
+            energy_penalty = -0.1 * energy_usage  # Small penalty for energy usage
+            reward += energy_penalty
         if self.task_finished == True :
             reward += 100
         
-        return reward                                                                                         # Distance to current waypoint, Smoothness of trajectory, Energy efficiency, task completion progress etc.
+        return reward                                                                                       # Distance to current waypoint, Smoothness of trajectory, Energy efficiency, task completion progress etc.
     
 
     def control_loop(self):                                                                                 # Main control Loop called by control timer
@@ -110,11 +125,6 @@ class VRXController(Node):
             return
         
         self.current_waypoint = self.waypoint_manager.get_current_waypoint()                                # current waypoint
-
-        #move to single custom message
-        '''#self.current_state = np.array(self.gps_data + self.imu_data + self.current_waypoint)                # Combines data into single State Array
-        #state_msg = Float32MultiArray(data=self.current_state.tolist())
-        #elf.state_pub.publish(state_msg)'''                                                                   # publishes state for DRL Agent
         
         if self.last_action is not None:
                                                                                                             # checks for available action
@@ -127,28 +137,23 @@ class VRXController(Node):
             self.right_thruster_pub.publish(right_thrust_msg)
             
         reward = self.get_reward()
-        #task_finished = (self.task_state == Task.STATE_FINISHED) #TODO implement task logic
+        
+        if ((self.gps_data != None) & (self.imu_data != None) & (self.current_waypoint != None)):
 
-        step_data = VRXStepData()
-        step_data.gps_data = self.gps_data
-        step_data.imu_data = self.imu_data
-        step_data.current_waypoint = self.current_waypoint
-        step_data.reward = reward
-        step_data.task_finished = self.task_finished
+            step_data = VRXStepData()
+            step_data.gps_data = self.gps_data
+            step_data.imu_data = self.imu_data
+            step_data.current_waypoint = self.current_waypoint
+            step_data.reward = reward
+            print("Reward: ", reward)
+            step_data.task_finished = self.task_finished
+            if self.task_finished == True:
+                print("task finished!")
 
-        step_data.heading_error = self.heading_error
-        step_data.current_heading = self.current_heading
-
-        self.step_publisher.publish(step_data)
-
-        #episode logic moved to DRL Agent 
-        '''#done = (self.episode_step >= self.max_steps) or (self.task_state == Task.STATE_FINISHED)            # get reward from reward function
-        #reward_msg = Float64MultiArray()
-        #reward_msg.data = [reward, 1.0 if done else 0.0]                                                    # Publishes reward For the DRL Agent
-        #self.reward_pub.publish(reward_msg)
-        #self.episode_step += 1
-        #if done:                                                                                            # incrementing episode count + done flag
-        #    self.reset_episode()'''
+            step_data.heading_error = self.heading_error
+            step_data.current_heading = self.current_heading
+        
+            self.step_publisher.publish(step_data)
     
     def reset_episode(self):                                                                            #TODO reset logic for reseting environment etc.
         self.episode_step = 0
