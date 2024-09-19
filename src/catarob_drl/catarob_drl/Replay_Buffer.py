6b6d9c994df427ebb5e7b80f1abe31b190ebdd9c
Dylan-Trows <TRWDYL001@myuct.ac.za>
2024-09-19 12:30:26 +0200

    changing nomenclature and comments

--- src/catarob_drl/catarob_drl/Replay_Buffer.py
+++ src/catarob_drl/catarob_drl/Replay_Buffer.py
@@ -10,26 +10,31 @@ class ReplayBuffer:
         self.state = np.zeros((self.max_size, state_dim))
         self.action = np.zeros((self.max_size, action_dim))
         self.next_state = np.zeros((self.max_size, state_dim))
-        self.reward = np.zeros((self.max_size, 1))
-        self.not_done = np.zeros((self.max_size, 1))
+        self.reward = np.zeros((self.max_size, 1))                  #np.zeros(self.max_size)
+        self.not_done = np.zeros((self.max_size, 1))                #np.zeros(self.max_size, dtype=np.bool)
 
     def add(self, state, action, next_state, reward, done):
         self.state[self.ptr] = state
         self.action[self.ptr] = action
         self.next_state[self.ptr] = next_state
         self.reward[self.ptr] = reward
-        self.not_done[self.ptr] = 1. - done
+        self.not_done[self.ptr] = 1. - done                         # use np.bool
 
-        self.ptr = (self.ptr + 1) % self.max_size
+        self.ptr = (self.ptr + 1) % self.max_size                   # loop back to the start of the array
         self.size = min(self.size + 1, self.max_size)
 
     def sample(self, batch_size):
-        ind = np.random.randint(0, self.size, size=batch_size)
+        batch = np.random.randint(0, self.size, size=batch_size)    #np.random.choice(self.size, batch_size)
 
+        # states = self.state[batch]
+        # next_states = self.next_state[batch]
+        # actions = self.action[batch]
+        # rewards = self.reward[batch]
+        # not_dones = self.not_done[batch]
         return (
-            torch.FloatTensor(self.state[ind]),
-            torch.FloatTensor(self.action[ind]),
-            torch.FloatTensor(self.next_state[ind]),
-            torch.FloatTensor(self.reward[ind]),
-            torch.FloatTensor(self.not_done[ind])
+            torch.FloatTensor(self.state[batch]),
+            torch.FloatTensor(self.action[batch]),
+            torch.FloatTensor(self.next_state[batch]),
+            torch.FloatTensor(self.reward[batch]),
+            torch.FloatTensor(self.not_done[batch])
         )
